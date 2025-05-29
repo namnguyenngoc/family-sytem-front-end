e94edea2d1f846b0ce6d0f41b5ca7c75d890e4b1
namnguyenngoc <itnguyennam@gmail.com>
2025-05-29 15:39:35 +0700

    test cicd

--- src/components/auth/sign-in-form.tsx
+++ src/components/auth/sign-in-form.tsx
@@ -72,7 +72,7 @@ export function SignInForm(): React.JSX.Element {
   return (
     <Stack spacing={4}>
       <Stack spacing={1}>
-        <Typography variant="h4">Sign in [Test 3]</Typography>
+        <Typography variant="h4">Sign in [Test 4]</Typography>
         <Typography color="text.secondary" variant="body2">
           Don&apos;t have an account?{' '}
           <Link component={RouterLink} href={paths.auth.signUp} underline="hover" variant="subtitle2">
