734b1b9927f0152596bc7599768159584bcac79a
namnguyenngoc <itnguyennam@gmail.com>
2025-05-30 16:28:57 +0700

    Add NEXT_PUBLIC_GRAPHQL_URI enviroment

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,8 +1,16 @@
-// src/lib/apollo-client.ts
 import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 
+const getGraphqlUri = () => {
+  if (typeof window !== 'undefined') {
+    // On client: use current domain
+    return `${window.location.protocol}//${window.location.host}/graphql`;
+  }
+  // On server: fallback or use env
+  return process.env.NEXT_PUBLIC_GRAPHQL_URI || 'http://localhost:9000/graphql';
+};
+
 const client = new ApolloClient({
-  uri: 'http://localhost:9000/graphql', // Replace with your GraphQL endpoint
+  uri: getGraphqlUri(),
   cache: new InMemoryCache(),
 });
 
